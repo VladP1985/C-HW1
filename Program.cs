2f67a0f6f8949b3a6c8e24c4719781766edd8b4e
VladP1985 <138965371+VladP1985@users.noreply.github.com>
2023-08-01 19:40:35 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,73 +1,67 @@
-﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
-// 456 -> 5
-// 782 -> 8
-// 918 -> 1
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
 
-// Console.WriteLine("Введите трехзначное число: ");
-// int num = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите число: ");
+//         string number = Console.ReadLine();
+//         int len = number.Length;
 
-// if (num < 100 || num > 999)
+//         if (len == 5)
+//         {
+//             if (number[0] == number[4] && number[1] == number[3])
+//             {
+//                 Console.WriteLine($"{number} - палиндром");
+//             }
+//             else
+//             {
+//                 Console.WriteLine($"{number} - не палиндром");
+//             }
+//         }
+//         else
+//         {
+//             Console.WriteLine($"ОШИБКА: {number} - не является пятизначным");
+//         }
+
+
+// Задача 21
+// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B(2, 1, -7), -> 15.84
+// A(7, -5, 0); B(1, -1, 9)-> 11.53
+
+// int x1 = ReadInt("Введите координату X первой точки: ");
+// int y1 = ReadInt("Введите координату Y первой точки: ");
+// int z1 = ReadInt("Введите координату Z первой точки: ");
+// int x2 = ReadInt("Введите координату X второй точки: ");
+// int y2 = ReadInt("Введите координату Y второй точки: ");
+// int z2 = ReadInt("Введите координату Z второй точки: ");
+
+// int A = x2 - x1;
+// int B = y2 - y1;
+// int C = z1 - z2;
+
+// double length = Math.Sqrt(A * A + B * B + C * C);
+// Console.WriteLine($"S= {length}");
+// int ReadInt(string message)
 // {
-// Console.WriteLine($"Некорректное трёхзначное число!");
-// }
-// else 
-// {  
-// Console.Write($"Вторая цифра: {num/10%10}");
+//     Console.Write(message);
+//     return Convert.ToInt32(Console.ReadLine());
 // }
 
+// Задача 23
+// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
 
+// int number = ReadInt("Введите число N: ");
 
-// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
-// 645 -> 5
-// 78 -> третьей цифры нет
-// 32679 -> 6
-// Решение подсмотрел, но разобрал
+// for (int i = 1; i <= number; i++)
+// { 
+//     Console.Write($"{i*i*i} ");
+// }
 
-int num = ReadInt("Введите число: ");
-int count = num.ToString().Length;
-Console.Write(MakeArray(num, count));
-
-int ReadInt(string message)
-{
-    Console.Write(message);
-    return Convert.ToInt32(Console.ReadLine());
-}
-int MakeArray(int a, int b)
-{
-int result = 0;
-    if (b < 3)
-    {
-        Console.Write("Третьей цифры нет");
-    }
-    else
-    {
-        int c = 1;
-        for (int i = b; i > 3; i--)
-        {
-            c=c*10;
-        }
-        result = (a/c)%10;
-    }
-return result;
-}
-
-
-// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
-// 6 -> да
-// 7 -> да
-// 1 -> нет
-
-// Console.WriteLine( "Введите цифру, обозначающую день недели:" );
-// int num = Convert.ToInt32(Console.ReadLine());
-// int day = num;
-// if (day == 6 || day == 7)
+// int ReadInt(string message)
 // {
-// Console.WriteLine("Выходной день");
+//     Console.Write(message);
+//     return Convert.ToInt32(Console.ReadLine());
 // }
-// else
-// {
-// Console.WriteLine($"Рабочий день");
-// }
-
-
-
